2ee541d4a5edafb2ef3b1c62bc0744d50d7ff7ad
goreleaser <goreleaser@goreleaser>
2017-04-20 20:04:33 -0300

    Releasing test version v2.1.7

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,9 +1,9 @@
 class Test < Formula
   desc "TODO"
   homepage "https://github.com/caarlos0/test"
-  url "https://github.com/caarlos0/test/releases/download/v2.1.3/test_macOS_64bits.tar.gz"
-  version "2.1.3"
-  sha256 "ad08f1729657215b26a33862ca607a0e8ea32308480631603ca67ab4eef4bebc"
+  url "https://github.com/caarlos0/test/releases/download/v2.1.7/test_macOS_64bits.tar.gz"
+  version "2.1.7"
+  sha256 "186aa26a374b311176178433cbb5a8e4806d550f17b3847701f09fc12b2d911c"
 
   def install
     bin.install "test"
