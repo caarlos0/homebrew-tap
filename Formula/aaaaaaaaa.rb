9cd2fdb56cf846a844c2ddab671379898522ce03
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2017-07-10 21:16:39 -0300

    Update aaaaaaaaa.rb

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -2,11 +2,12 @@ class Aaaaaaaaa < Formula
   desc ""
   homepage ""
   url "https://github.com/caarlos0/test/releases/download/v4.6.5/moises_4.6.5_macOS_64bits"
-  version "4.5.6"
+  version "4.5.7"
   sha256 "fabd56eaf8d27520431ccca5132f7194bfbef238efee7714803a97561c05dd67"
 
   def install
     bin.install "moises_4.6.5_macOS_64bits"
+    mv bin/"moises_4.6.5_macOS_64bits", bin/"moises"
   end
 
   def caveats
