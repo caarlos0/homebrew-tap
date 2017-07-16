ef8c403daa3f0f533385fbaa1f3fb9240921ff49
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-16 11:59:31 -0300

    aaaaaaaaa version v12.0.0

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -1,16 +1,20 @@
 class Aaaaaaaaa < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/v10.0.2/aaaaaaaaa_10.0.2_macOS_64bits.tar.gz"
-  version "10.0.2"
-  sha256 "0a89b48aca6138d46d6a2df859bc1098dc9ae3e8459cecda1992230311864058"
+  url "https://github.com/caarlos0/test/releases/download/v12.0.0/aaaaaaaaa_12.0.0_macOS_64bits.tar.gz"
+  version "12.0.0"
+  sha256 "87a416893cad05318de858ffa0352872b50e7763d7af6b8e28d1182bae9a27fc"
 
   def install
     bin.install "moises"
     bin.install "foo"
   end
-
   def caveats
     "Test formula, dont use it. It doesnt do anything"
+  end
+  def test
+    system "#{bin}/foo"
+system "#{bin}/moises"
+
   end
 end
