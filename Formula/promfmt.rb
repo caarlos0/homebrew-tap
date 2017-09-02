60d4aca25a64acc355d730b6eb33a53dc1b90e8d
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-02 11:12:45 -0300

    promfmt version v1.1.0

--- Formula/promfmt.rb
+++ Formula/promfmt.rb
@@ -1,9 +1,9 @@
 class Promfmt < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/promfmt/releases/download/v1.0.0/promfmt_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "28629c3af9d2df5939f4e29901d4304c30f26eae64514a6a3a686d94e901c023"
+  url "https://github.com/caarlos0/promfmt/releases/download/v1.1.0/promfmt_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "03dcfed6b37bd1ad1e4924c01534d4a9a3e38ef5678bf37c232a50eed4d5da90"
 
   def install
     bin.install "promfmt"
