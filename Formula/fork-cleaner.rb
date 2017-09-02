bff607027bde6fa88d23a5c59b3c9f7e4454718e
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-02 18:48:01 -0300

    fork-cleaner version v1.3.0

--- Formula/fork-cleaner.rb
+++ Formula/fork-cleaner.rb
@@ -1,11 +1,15 @@
 class ForkCleaner < Formula
-  desc "Cleans up old and inactive forks on your github account."
-  homepage "https://github.com/caarlos0/fork-cleaner"
-  url "https://github.com/caarlos0/fork-cleaner/releases/download/v1.2.0/fork-cleaner_Darwin_x86_64.tar.gz"
-  version "1.2.0"
-  sha256 "3f955d7b0099c22e9af2ecb53fee5d699e9d8699ecbf967befad82ff1ff1ed0a"
+  desc ""
+  homepage ""
+  url "https://github.com/caarlos0/fork-cleaner/releases/download/v1.3.0/fork-cleaner_1.3.0_darwin_amd64.tar.gz"
+  version "1.3.0"
+  sha256 "037c50049a48ca7abd6cf5464f86a6c9a1742ee444875a0f62ea20bdc5674291"
 
   def install
     bin.install "fork-cleaner"
+  end
+
+  test do
+    
   end
 end
