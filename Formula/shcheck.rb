d7e61a4322635c86a3ebe65912fff9c9223ec225
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-21 23:34:49 -0300

    shcheck version v0.3.0

--- Formula/shcheck.rb
+++ Formula/shcheck.rb
@@ -1,9 +1,9 @@
 class Shcheck < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/shcheck/releases/download/v0.2.1/shcheck_Darwin_x86_64.tar.gz"
-  version "0.2.1"
-  sha256 "bb55a7e70f5db7ea722a194d757e0a07720d25af79876cefe8e5f9b856089e3a"
+  url "https://github.com/caarlos0/shcheck/releases/download/v0.3.0/shcheck_Darwin_x86_64.tar.gz"
+  version "0.3.0"
+  sha256 "2d0ea0ca76c95e45ef1b354331b27ea7bddc40adfd63529351d51f90a7d4f53f"
 
   def install
     bin.install "shcheck"
