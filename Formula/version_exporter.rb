e39f4d400112d67f8aa496ea0d56563048e4e284
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-05 11:09:29 -0300

    version_exporter version v0.0.2

--- Formula/version_exporter.rb
+++ Formula/version_exporter.rb
@@ -1,9 +1,9 @@
 class VersionExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/version_exporter/releases/download/v0.0.1/version_exporter_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "b8b554e5b3972d55684252a8a44db2778a5eb6077e53d7dee79043fcaf177ab9"
+  url "https://github.com/caarlos0/version_exporter/releases/download/v0.0.2/version_exporter_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "302128579387d2e44130d5561dc30b6a9f91f0fa0f0d5c2257e6abb251ba625e"
 
   def install
     bin.install "version_exporter"
