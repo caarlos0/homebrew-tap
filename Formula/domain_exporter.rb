b1fc4e8e98eed18d7d3332d69b9150166aa6a0d0
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-21 14:19:15 -0200

    domain_exporter version v1.0.5

--- Formula/domain_exporter.rb
+++ Formula/domain_exporter.rb
@@ -1,9 +1,9 @@
 class DomainExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/domain_exporter/releases/download/v1.0.4/domain_exporter_darwin_amd64.tar.gz"
-  version "1.0.4"
-  sha256 "ae956fb7dd6bad6bbee098ae7b813390773def6f366ddd8aadd6bab7ecf2b06a"
+  url "https://github.com/caarlos0/domain_exporter/releases/download/v1.0.5/domain_exporter_darwin_amd64.tar.gz"
+  version "1.0.5"
+  sha256 "e3e9234832ba362f4142643d451b131c7901ea976750fcc87630af076da1bb6f"
 
   def install
     bin.install "domain_exporter"
