c98a11c0fd667e2578605386bba302987988903c
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-10 10:55:27 -0200

    karmahub version v2.0.2

--- Formula/karmahub.rb
+++ Formula/karmahub.rb
@@ -1,9 +1,9 @@
 class Karmahub < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/karmahub/releases/download/v2.0.1/karmahub_2.0.1_darwin_amd64.tar.gz"
-  version "2.0.1"
-  sha256 "65f7e8051fcb9eed1cb15502f167f73b7a12d8034c96eb282d845df3ff35e961"
+  url "https://github.com/caarlos0/karmahub/releases/download/v2.0.2/karmahub_2.0.2_darwin_amd64.tar.gz"
+  version "2.0.2"
+  sha256 "edcce0c0138cd4cee98a775e8767eaf64766272e279559e32f71912c764914a6"
 
   def install
     bin.install "karmahub"
