02cfda95be1d1aa55e3113228c5648cfcc7392a9
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-16 20:21:07 -0200

    github-vacations version v2.0.0

--- Formula/github-vacations.rb
+++ Formula/github-vacations.rb
@@ -1,11 +1,15 @@
 class GithubVacations < Formula
-  desc "Automagically ignore all notifications related to work when you are on vacations"
-  homepage "https://github.com/caarlos0/github-vacations"
-  url "https://github.com/caarlos0/github-vacations/releases/download/v1.0.5/github-vacations_Darwin_x86_64.tar.gz"
-  version "1.0.5"
-  sha256 "1d92a2fcd81e7d5fbfbac80b909027d16f33a2a1ac344b9700f28b541122dd26"
+  desc ""
+  homepage ""
+  url "https://github.com/caarlos0/github-vacations/releases/download/v2.0.0/github-vacations_2.0.0_darwin_amd64.tar.gz"
+  version "2.0.0"
+  sha256 "e2c53a3dbc723a09e85e5e891e5eebd2380133beec623a049598eb0bbc80ef50"
 
   def install
     bin.install "github-vacations"
+  end
+
+  test do
+    
   end
 end
