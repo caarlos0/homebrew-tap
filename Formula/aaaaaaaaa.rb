95f531cc27698acc683969df826fb4e49496822c
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-17 23:16:19 -0200

    aaaaaaaaa version v7.0.18

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -1,9 +1,9 @@
 class Aaaaaaaaa < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/v6.0.0/aaaaaaaaa_6.0.0_macOS_64bits.tar.gz"
-  version "6.0.0"
-  sha256 "31b5329c5171707a0ba0e99c93f7991ab4a8a24c178c19d39a6b25b4ffb0c9ba"
+  url "https://github.com/caarlos0/test/releases/download/v7.0.18/aaaaaaaaa-7.0.18-<no value>-macOS_64bits.tar.gz"
+  version "7.0.18"
+  sha256 "7ef56b362628ff65f5fd81157aa26787bff79d5b3925e42d34ee30e61844c6f5"
 
   def install
     bin.install "moises"
