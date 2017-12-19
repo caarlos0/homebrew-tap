4f34fba3a8e5af15d553325a4b51ae324dc0a7b8
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-19 16:18:28 -0200

    svu version v0.0.4

--- Formula/svu.rb
+++ Formula/svu.rb
@@ -1,9 +1,9 @@
 class Svu < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/svu/releases/download/v0.0.3/svu_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "3a2ffa5776eb2caacc1b64973936e175072ab7a6eb5f5144a40a700d892adf94"
+  url "https://github.com/caarlos0/svu/releases/download/v0.0.4/svu_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "0e13d2d944288a2255d0e7341a4c94b46755a5de0fa1437b2fe3b820d8e03936"
 
   def install
     bin.install "svu"
