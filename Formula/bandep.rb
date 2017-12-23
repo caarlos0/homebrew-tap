2e322c8111f6583b2f7e723606b6d207173a08be
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-23 11:50:51 -0200

    bandep version v0.1.0

--- Formula/bandep.rb
+++ Formula/bandep.rb
@@ -1,9 +1,9 @@
 class Bandep < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/bandep/releases/download/v0.0.1/bandep_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "b97243369610e9c8c8ceea63d992ae60a7caa1f20de1ff50b9805c900f6b5744"
+  url "https://github.com/caarlos0/bandep/releases/download/v0.1.0/bandep_0.1.0_darwin_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "cd75a0f71624f4ff40a5fe94c985147b9b856664b65295a2fbeda2534be892f5"
 
   def install
     bin.install "bandep"
