2cf093e8dbf679bac2cf1910c42d42e484eb215a
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-16 21:27:48 -0200

    github-vacations version v2.0.1

--- Formula/github-vacations.rb
+++ Formula/github-vacations.rb
@@ -1,9 +1,9 @@
 class GithubVacations < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/github-vacations/releases/download/v2.0.0/github-vacations_2.0.0_darwin_amd64.tar.gz"
-  version "2.0.0"
-  sha256 "e2c53a3dbc723a09e85e5e891e5eebd2380133beec623a049598eb0bbc80ef50"
+  url "https://github.com/caarlos0/github-vacations/releases/download/v2.0.1/github-vacations_2.0.1_darwin_amd64.tar.gz"
+  version "2.0.1"
+  sha256 "1198733a3774f50c89650cf4ae3224bbe4d2cce7de679074e59221763bf7118d"
 
   def install
     bin.install "github-vacations"
