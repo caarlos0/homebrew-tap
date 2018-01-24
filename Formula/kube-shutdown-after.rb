6df4a352c6c18741fcfc4eba03b06f4c2d7a30f8
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-24 15:36:13 -0200

    kube-shutdown-after version v0.1.1

--- Formula/kube-shutdown-after.rb
+++ Formula/kube-shutdown-after.rb
@@ -1,9 +1,9 @@
 class KubeShutdownAfter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/kube-shutdown-after/releases/download/v0.1.0/kube-shutdown-after_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "68434bd67a8e9ebaa83f85ff77478723f01b32e0cc4473b50fed9e15a90bd5be"
+  url "https://github.com/caarlos0/kube-shutdown-after/releases/download/v0.1.1/kube-shutdown-after_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "1fbd06295f6cc9f981baca54d7f6989b42f5839495c6d36b5803f8e3dbaa4296"
 
   def install
     bin.install "kube-shutdown-after"
