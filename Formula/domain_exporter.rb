f4e236fbe4639ab44d0e07041fc7df216dcf86ab
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-03 20:55:45 -0300

    Brew formula update for domain_exporter version v1.0.9

--- Formula/domain_exporter.rb
+++ Formula/domain_exporter.rb
@@ -1,9 +1,9 @@
 class DomainExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/domain_exporter/releases/download/v1.0.8/domain_exporter_darwin_amd64.tar.gz"
-  version "1.0.8"
-  sha256 "e981bfc5805cf6e74c6668c79d9e8a6a1465f2e279ea367b2fb0aa28157d076f"
+  url "https://github.com/caarlos0/domain_exporter/releases/download/v1.0.9/domain_exporter_darwin_amd64.tar.gz"
+  version "1.0.9"
+  sha256 "2716d39ec89c39110af3ef7c0b5c360339d3d61c6274b0e2a669c1e8217e97f8"
 
   def install
     bin.install "domain_exporter"
