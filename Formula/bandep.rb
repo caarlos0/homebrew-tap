31afa826e91e16ffe7367e82c518c7d3a8b22dd2
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-24 21:38:51 -0700

    Brew formula update for bandep version v0.1.3

--- Formula/bandep.rb
+++ Formula/bandep.rb
@@ -1,15 +1,11 @@
 class Bandep < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/bandep/releases/download/v0.1.2/bandep_0.1.2_darwin_amd64.tar.gz"
-  version "0.1.2"
-  sha256 "1d062eaaf1cefc3e9635ce62f822f233f5e2ae81a7375757712b2295512b1727"
+  url "https://github.com/caarlos0/bandep/releases/download/v0.1.3/bandep_0.1.3_darwin_amd64.tar.gz"
+  version "0.1.3"
+  sha256 "b79cabb3ef0a2ebb8ee1178372be42762689b4d60cbb3bb2a45635c9a81d6434"
 
   def install
     bin.install "bandep"
-  end
-
-  test do
-    
   end
 end
