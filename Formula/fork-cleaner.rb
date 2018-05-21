3cfafee967571f0a291cf06b5cafcc38117a37a0
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-21 08:47:44 -0300

    Brew formula update for fork-cleaner version v1.4.1

--- Formula/fork-cleaner.rb
+++ Formula/fork-cleaner.rb
@@ -1,9 +1,9 @@
 class ForkCleaner < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/caarlos0/fork-cleaner/releases/download/v1.4.0/fork-cleaner_1.4.0_darwin_amd64.tar.gz"
-  version "1.4.0"
-  sha256 "2fc97bef5f3c4da476c1e7e782013c21f7291bb13bec31a905f63ae0c0351c49"
+  desc "Cleans up old and inactive forks on your github account."
+  homepage "https://github.com/caarlos0/fork-cleaner"
+  url "https://github.com/caarlos0/fork-cleaner/releases/download/v1.4.1/fork-cleaner_1.4.1_darwin_amd64.tar.gz"
+  version "1.4.1"
+  sha256 "89498b28fcc62ab1789f420bff4d5db9610a57de50cfdc58276fcf251f9052f7"
 
   def install
     bin.install "fork-cleaner"
