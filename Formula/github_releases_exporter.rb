6efd28ddb3f4c25a8d0f413506015521c039f529
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-30 09:46:00 -0300

    Brew formula update for github_releases_exporter version v1.0.2

--- Formula/github_releases_exporter.rb
+++ Formula/github_releases_exporter.rb
@@ -1,9 +1,9 @@
 class GithubReleasesExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/github_releases_exporter/releases/download/v1.0.1/github_releases_exporter_darwin_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "3e3850d50723f021e1ab3e2e609f97c05ff4d6f7f474e016c081e38b7cced7a7"
+  url "https://github.com/caarlos0/github_releases_exporter/releases/download/v1.0.2/github_releases_exporter_darwin_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "83e0eb6bf43f3272ae2431a0ce6cf24a7f3c4cba2174742440483fb15ee51735"
 
   def install
     bin.install "github_releases_exporter"
