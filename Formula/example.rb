139e5c54520608100d7f6f4847bbd0e5cabab55b
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-30 13:47:12 -0300

    Brew formula update for example version v0.2.0

--- Formula/example.rb
+++ Formula/example.rb
@@ -1,9 +1,9 @@
 class Example < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/example/releases/download/v0.1.0/example_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "835f622fe8e428d7e46d55081e5e27284e519aa745e9ac8cb7414a15c7fd9951"
+  url "https://github.com/caarlos0/example/releases/download/v0.2.0/example_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "b4f019d1a7414c9704c956b7b2c1fd7a413384f5781e501d38a482fda215d245"
 
   def install
     bin.install "example"
