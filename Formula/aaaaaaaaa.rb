9da29770f4bb3372045d9e88733a64406eac8e0e
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-30 10:08:34 -0300

    Brew formula update for aaaaaaaaa version v7.0.23

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -1,17 +1,18 @@
 class Aaaaaaaaa < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/v7.0.19/aaaaaaaaa-7.0.19-go1.9.2-macOS_64bits.tar.gz"
-  version "7.0.19"
-  sha256 "2a750533cd4ba5126b9ee6ed40c26fcea38c3c618b963d5506e1ed9f8382f986"
+  url "https://github.com/caarlos0/test/releases/download/v7.0.23/aaaaaaaaa-SNAPSHOT-5e08238951d970a27d56776344eda16df05e99f8-1.11-macOS_64bits.tar.gz"
+  version "SNAPSHOT-5e08238951d970a27d56776344eda16df05e99f8"
+  sha256 "5661b1aa4c24135708ef5f3a1ff0155be518c83327bc6345d77fc7cd2eca3908"
 
   def install
     bin.install "moises"
     bin.install "foo"
   end
 
-  def caveats
-    "Test formula, dont use it. It doesnt do anything"
+  def caveats; <<~EOS
+    Test formula, dont use it. It doesnt do anything
+  EOS
   end
 
   test do
