e7df6e03b3de2843dbb384cab190576f846ebfd1
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-30 10:12:57 -0300

    Brew formula update for aaaaaaaaa version v7.0.23

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -1,9 +1,9 @@
 class Aaaaaaaaa < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/v7.0.23/aaaaaaaaa-SNAPSHOT-5e08238951d970a27d56776344eda16df05e99f8-1.11-macOS_64bits.tar.gz"
-  version "SNAPSHOT-5e08238951d970a27d56776344eda16df05e99f8"
-  sha256 "5661b1aa4c24135708ef5f3a1ff0155be518c83327bc6345d77fc7cd2eca3908"
+  url "https://github.com/caarlos0/test/releases/download/v7.0.23/aaaaaaaaa-SNAPSHOT-81b23ade4812421d1dc6d17558126beea309be41-1.11-macOS_64bits.tar.gz"
+  version "SNAPSHOT-81b23ade4812421d1dc6d17558126beea309be41"
+  sha256 "63d5a4655afc34b8c4847ec063c016bddbbdfb1ac5c2cb00a05401de49a40bab"
 
   def install
     bin.install "moises"
