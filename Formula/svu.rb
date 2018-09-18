735fa6b83e7494e5ec3ab2cf0ac76bf6da738866
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-18 13:41:38 -0300

    Brew formula update for svu version v0.1.0

--- Formula/svu.rb
+++ Formula/svu.rb
@@ -1,15 +1,11 @@
 class Svu < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/svu/releases/download/v0.0.4/svu_0.0.4_darwin_amd64.tar.gz"
-  version "0.0.4"
-  sha256 "0e13d2d944288a2255d0e7341a4c94b46755a5de0fa1437b2fe3b820d8e03936"
+  url "https://github.com/caarlos0/svu/releases/download/v0.1.0/svu_0.1.0_darwin_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "b8fea190044916a44eb57e3067336705bd730c98e06e75dfa516a3ab563cfdd6"
 
   def install
     bin.install "svu"
-  end
-
-  test do
-    
   end
 end
