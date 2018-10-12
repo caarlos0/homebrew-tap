b26efec590ddd32d07365eba19f3c6727ef17b85
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-12 00:21:32 -0300

    Brew formula update for aaaaaaaaa version 5.4.0

--- Formula/aaaaaaaaa.rb
+++ Formula/aaaaaaaaa.rb
@@ -1,9 +1,9 @@
 class Aaaaaaaaa < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/5.3.4/aaaaaaaaa-5.3.4-1.11-macOS_64bits.tar.gz"
-  version "5.3.4"
-  sha256 "79ae7bb979c92ac63388a889ed22b4799d81c3b3032d2a79fb2a5e405b86edfa"
+  url "https://github.com/caarlos0/test/releases/download/5.4.0/aaaaaaaaa-5.4.0-1.11-macOS_64bits.tar.gz"
+  version "5.4.0"
+  sha256 "2b9a04fbef6d83988cb8b6f3a08ccd2a74c301f91a997a1672b61222ba983117"
 
   def install
     bin.install "moises"
