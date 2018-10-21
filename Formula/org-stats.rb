6884f37713934da79cee58829a815cc0c9b6f1c7
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-21 12:26:01 -0300

    Brew formula update for org-stats version v1.4.1

--- Formula/org-stats.rb
+++ Formula/org-stats.rb
@@ -1,9 +1,9 @@
 class OrgStats < Formula
   desc "Get the contributor stats summary from all repos of any given organization"
   homepage ""
-  url "https://github.com/caarlos0/org-stats/releases/download/v1.4.0/org-stats_1.4.0_darwin_amd64.tar.gz"
-  version "1.4.0"
-  sha256 "5452456400b683899352e03b54eaf524e5da15575f2d8f26db377beba4200bec"
+  url "https://github.com/caarlos0/org-stats/releases/download/v1.4.1/org-stats_1.4.1_darwin_amd64.tar.gz"
+  version "1.4.1"
+  sha256 "4c74967fa29ec242c86c6d6300db4eb2d1d47c5250fda0c5d8e0363f480af800"
 
   def install
     bin.install "org-stats"
