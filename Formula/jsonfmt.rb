6170591de7ef147fd41cecb5d371dbf9e2644ab5
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-09 15:41:00 -0200

    Brew formula update for jsonfmt version v0.2.0

--- Formula/jsonfmt.rb
+++ Formula/jsonfmt.rb
@@ -1,9 +1,9 @@
 class Jsonfmt < Formula
   desc "Like gofmt, but for JSON files"
   homepage "https://carlosbecker.com"
-  url "https://github.com/caarlos0/jsonfmt/releases/download/v0.1.0/jsonfmt_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "1777a8441fd19783a830a8136c06152f0e6527a880cc3c0d6bcc828a9f019d52"
+  url "https://github.com/caarlos0/jsonfmt/releases/download/v0.2.0/jsonfmt_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "3d6353c4c9d1aec24bcbd951040e826cdb00578d1fe1f45e5c449d75e0eb395b"
 
   def install
     bin.install "jsonfmt"
