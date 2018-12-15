078b8f632792ed3e08bf447b0d34524c5229b05d
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-15 20:05:07 -0200

    Brew formula update for version_exporter version v1.0.4

--- Formula/version_exporter.rb
+++ Formula/version_exporter.rb
@@ -1,9 +1,9 @@
 class VersionExporter < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/version_exporter/releases/download/v1.0.3/version_exporter_darwin_amd64.tar.gz"
-  version "1.0.3"
-  sha256 "965f92443eca83cf2c5ea8306096f3a8a6fd9c30e94142980a6fd29d368e1d31"
+  url "https://github.com/caarlos0/version_exporter/releases/download/v1.0.4/version_exporter_darwin_amd64.tar.gz"
+  version "1.0.4"
+  sha256 "b1db5d5b836c0d2fdcdd9f9fb3919b34968ac54642c2d1a2d77f8a7b5cc5be3b"
 
   def install
     bin.install "version_exporter"
