994784edcc388a88c9762680d864df77bcb65984
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2019-02-12 13:47:22 -0200

    Update gssh.rb

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -2,7 +2,7 @@ class Gssh < Formula
   desc "quickly ssh into gcloud instances"
   homepage "https://github.com/caarlos0/gssh"
   url "https://raw.githubusercontent.com/caarlos0/gssh/master/gssh"
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "jq"
   depends_on "fzf"
