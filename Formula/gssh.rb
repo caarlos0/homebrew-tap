ba4414cafba2c566c14550bdbd966d2abd3f8c7d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2019-02-12 11:09:53 -0200

    added: gssh

---
+++ Formula/gssh.rb
@@ -0,0 +1,19 @@
+class Gssh < Formula
+  desc "quickly ssh into gcloud instances"
+  homepage "https://github.com/caarlos0/gssh"
+  url "https://raw.githubusercontent.com/caarlos0/gssh/master/gssh"
+  version "0.0.1"
+
+  depends_on "jq"
+  depends_on "fzf"
+  # TODO: this is cask, how to make this work?
+  # depends_on "google-cloud-sdk"
+
+  def install
+    bin.install "gssh"
+  end
+
+  test do
+    system "gssh -h"
+  end
+end
