d6cf4a6cffb402a5909eb3788479b132ad29f34d
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-15 17:47:01 -0300

    Brew formula update for clone-org version v1.1.2

--- Formula/clone-org.rb
+++ Formula/clone-org.rb
@@ -2,16 +2,16 @@
 class CloneOrg < Formula
   desc ""
   homepage ""
-  version "1.1.1"
+  version "1.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/caarlos0/clone-org/releases/download/v1.1.1/clone-org_1.1.1_darwin_amd64.tar.gz"
-    sha256 "ab11c198b05ea5e196ab9e21c9ba89325849da600a6be88972a3b08080ccd282"
+    url "https://github.com/caarlos0/clone-org/releases/download/v1.1.2/clone-org_1.1.2_darwin_amd64.tar.gz"
+    sha256 "b0ae2173af4ae16a130f11ca20b63efd8360574569f5ef199a37f98341398e8c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/clone-org/releases/download/v1.1.1/clone-org_1.1.1_linux_amd64.tar.gz"
-      sha256 "44009eaa33c68f141cb5f662bdce3f58e055b02f861d51c10a5fe689b89ba2be"
+      url "https://github.com/caarlos0/clone-org/releases/download/v1.1.2/clone-org_1.1.2_linux_amd64.tar.gz"
+      sha256 "65f097c0e6523ff4bfed6a1c924db33af7ecc2765192cf868a86d36f5c4c02e2"
     end
   end
 
