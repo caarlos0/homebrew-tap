4b21601269958f3f8ea557094bdd3c28a2cd019e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2020-10-15 16:14:57 -0300

    fix: update gssh to 1.1.0

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -1,10 +1,10 @@
 class Gssh < Formula
   desc "quickly ssh into gcloud instances"
   homepage "https://github.com/caarlos0/gssh"
-  url "https://raw.githubusercontent.com/caarlos0/gssh/v1.0.2/gssh"
-  version "1.0.2"
+  url "https://raw.githubusercontent.com/caarlos0/gssh/v1.1.0/gssh"
+  version "1.1.0"
   # revision 1
-  sha256 "a0ffbee9e7ec6c2b8f0680564f27b6ed7d713642a5ec8a663e3df6c561a00401"
+  sha256 "2df45dbb7313f387c58865b0d4bd854cc006e812943aacd1bdaef61a63688a97"
 
   depends_on "jq"
   depends_on "fzf"
