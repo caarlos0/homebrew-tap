4133c7b7fa6ac39dc0b719f2794829f66526d32b
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-16 14:14:11 -0300

    Brew formula update for kubectx version v0.9.5

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -2,24 +2,24 @@
 class Kubectx < Formula
   desc "kubectx and kubens are power tools for kubectl"
   homepage "https://kubectx.dev"
-  version "0.9.4"
+  version "0.9.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/caarlos0/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_darwin_x86_64.tar.gz"
-    sha256 "df24c1552d9109f03b316f55350447d03d17ff85905e78a9eac9de63c39a6f71"
+    url "https://github.com/caarlos0/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_darwin_x86_64.tar.gz"
+    sha256 "1992fd5001a8df9d2f21440d81a6b5ddc491c67c5b1d4c3bd3b2cf3e6742e1c7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz"
-      sha256 "2e98229dbeb9663b969e2ddd8dd3309220a9059c3915215f5cffba68a5db40be"
+      url "https://github.com/caarlos0/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_x86_64.tar.gz"
+      sha256 "6823a54ade980080e33212f5c4c51abe781333b67646df58ca6f53e6a089f349"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/caarlos0/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_arm64.tar.gz"
-        sha256 "a3b3aa6d79a98012348e4ea344e945d9b33bbd44f980ef3d440d91c0239a659e"
+        url "https://github.com/caarlos0/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_arm64.tar.gz"
+        sha256 "080d9baff8044433a183452a19a2a4be3327c99e08bef39894da10c86d48ddc0"
       else
-        url "https://github.com/caarlos0/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_armhf.tar.gz"
-        sha256 "fff4560b6d5d585f9fa627bf1ed94ddd15ae8a3d79902fe967f9be2d111fb4e9"
+        url "https://github.com/caarlos0/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_armhf.tar.gz"
+        sha256 "a15b547d8dce470b906cb252ab0c0817d3602291c2760d0a5739d4385fb2833f"
       end
     end
   end
