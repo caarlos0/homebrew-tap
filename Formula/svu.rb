a494c4dd5c1a5ba199b6e4ea51d1d497cbb07182
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-09 17:27:22 -0300

    Brew formula update for svu version v1.3.2

--- Formula/svu.rb
+++ Formula/svu.rb
@@ -5,16 +5,16 @@
 class Svu < Formula
   desc ""
   homepage ""
-  version "1.3.1"
+  version "1.3.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/caarlos0/svu/releases/download/v1.3.1/svu_1.3.1_darwin_amd64.tar.gz"
-    sha256 "c7e6f003cfdc12f7c89a065f6ef6c6553b136b5fd1cea328e6efef3b98ecb078"
+    url "https://github.com/caarlos0/svu/releases/download/v1.3.2/svu_1.3.2_darwin_amd64.tar.gz"
+    sha256 "56bef50fa30588839f7a847f0ad86ff980dc00e7d02c50c515c88dc19030c636"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/caarlos0/svu/releases/download/v1.3.1/svu_1.3.1_linux_amd64.tar.gz"
-    sha256 "cfb609611a283586782003a168e0c15efb74b5a3225a1e3f1f0e05f0d69d4231"
+    url "https://github.com/caarlos0/svu/releases/download/v1.3.2/svu_1.3.2_linux_amd64.tar.gz"
+    sha256 "84150c3f825e52b431e10d19eeb90a2fc978a6c8247369503376ee7615f01b5c"
   end
 
   def install
