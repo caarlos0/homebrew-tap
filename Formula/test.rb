088f6c464a655bac75b4afe1b694b09e8b0beb23
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-05-26 22:54:19 -0300

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,7 +1,6 @@
 class Test < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/v4.0.7/test_4.0.7_macOS_64bits.binary"
   version "4.0.7"
   sha256 "8b4e4108c5fcee30327ec74a6b560da8ef00e614ebf5a876c0a83797a928c3c7"
 
@@ -14,7 +13,7 @@ class Test < Formula
     "Test formula, dont use it. It doesnt do anything"
   end
   
-  depends_on [:linux, :macos]
+  # depends_on [:linux, :macos]
   
   on_macos do
     depends_on arch: [:aarch64, :x86_64]
@@ -22,5 +21,6 @@ class Test < Formula
   
   on_linux do
     depends_on arch: [:x86_64]
+    url "https://github.com/caarlos0/test/releases/download/v4.0.7/test_4.0.7_macOS_64bits.binary"
   end
 end
