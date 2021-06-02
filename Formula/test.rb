200ac4e47bcebee96f3f4523d50327d20fafe099
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-06-02 15:12:17 -0300

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,26 +1,61 @@
+class SupportedArchRequirement < Requirement
+  fatal true
+
+  satisfy(build_env: false) { self.class.supported_arch? }
+
+  def message
+    "Your platform #{self.class.plat} is not supported by this tap. Supported arches are #{self.class.supported_arches}."
+  end
+
+  LINUX_SUPPORTED_ARCHES = [:x86_64, :arm, :aarch64]
+  MACOS_SUPPORTED_ARCHES = [:aarch64]
+
+  def self.supported_arch?
+    self.supported_arches.include? self.plat
+  end
+
+  def self.supported_arches
+    return LINUX_SUPPORTED_ARCHES if OS.linux?
+    return MACOS_SUPPORTED_ARCHES if OS.mac?
+    []
+  end
+
+  def self.plat
+    case RUBY_PLATFORM
+    when /x86_64/ then :x86_64
+    when /arm/ then :arm
+    when /aarch64/ then :aarch64
+    else :dunno
+    end
+  end
+end
+
+
 class Test < Formula
-  desc ""
-  homepage ""
-  version "4.0.7"
-  sha256 "8b4e4108c5fcee30327ec74a6b560da8ef00e614ebf5a876c0a83797a928c3c7"
+  desc "Test etc"
+  homepage "https://example.com"
+  version "1.0.0"
+  sha256 "aaaaaa"
+  url "https://fake.url/nope"
+
+  depends_on SupportedArchRequirement
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://fake.url/macos/arm"
+    end
+  end
+
+  on_linux do
+    url "https://fake.url/linux/all"
+  end
 
   def install
-    bin.install "moises"
-    bin.install "foo"
+    bin.install "test"
   end
 
   def caveats
     "Test formula, dont use it. It doesnt do anything"
   end
-  
-  # depends_on [:linux, :macos]
-  
-  on_macos do
-    depends_on arch: [:aarch64, :x86_64]
-  end
-  
-  on_linux do
-    depends_on arch: [:x86_64]
-    url "https://github.com/caarlos0/test/releases/download/v4.0.7/test_4.0.7_macOS_64bits.binary"
-  end
 end
+
