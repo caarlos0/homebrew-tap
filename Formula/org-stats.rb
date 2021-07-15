e556513127022400ff7aba4a69ca5a23cf7ab18a
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-14 23:26:50 -0300

    Brew formula update for org-stats version v1.8.0

--- Formula/org-stats.rb
+++ Formula/org-stats.rb
@@ -5,28 +5,28 @@
 class OrgStats < Formula
   desc "Get the contributor stats summary from all repos of any given organization"
   homepage "https://github.com/caarlos0/org-stats"
-  version "1.7.0"
+  version "1.8.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/org-stats/releases/download/v1.7.0/org-stats_darwin_amd64.tar.gz"
-      sha256 "fe384731082e9db2a81ee40f76421d6190371ca28fc757c8059c15b5a13063af"
+      url "https://github.com/caarlos0/org-stats/releases/download/v1.8.0/org-stats_darwin_amd64.tar.gz"
+      sha256 "7d29208c5ed8ba8eb6cac1daaf2fddc8e530df2c52524e9f1eace910a695f1f3"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/caarlos0/org-stats/releases/download/v1.7.0/org-stats_darwin_arm64.tar.gz"
-      sha256 "7d876a2338c683c4055231e30602112d535e3928e3510bc41b307afb3a2afaae"
+      url "https://github.com/caarlos0/org-stats/releases/download/v1.8.0/org-stats_darwin_arm64.tar.gz"
+      sha256 "67c29d2dc997cc0b94896eaf49f55ceabd587d63e097f39b573924f35dcc9ee6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/org-stats/releases/download/v1.7.0/org-stats_linux_amd64.tar.gz"
-      sha256 "46addc5ab929b1ab51e2c704112374505a9a12e38cef7699440a5cfffd23cf2f"
+      url "https://github.com/caarlos0/org-stats/releases/download/v1.8.0/org-stats_linux_amd64.tar.gz"
+      sha256 "f330b7e94878f6d41ebd20954a8e877528537853f300eb333c988a6f67c6ac47"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/caarlos0/org-stats/releases/download/v1.7.0/org-stats_linux_arm64.tar.gz"
-      sha256 "09140af64e7ec41a152a2ee03c50a0e7aaeb28254db917d6be98c5bde3a9f04e"
+      url "https://github.com/caarlos0/org-stats/releases/download/v1.8.0/org-stats_linux_arm64.tar.gz"
+      sha256 "ef03d9b54af7e69b809de275043cec1ca3d642083386652212f06342a18bcdc3"
     end
   end
 
