93153c7ba1346345d1858b1af0ac0b4658a2e5f7
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-04 09:36:55 -0300

    Brew formula update for svu version v1.7.0

--- Formula/svu.rb
+++ Formula/svu.rb
@@ -5,28 +5,28 @@
 class Svu < Formula
   desc "Semantic Version Utility"
   homepage "https://github.com/caarlos0/svu"
-  version "1.6.1"
+  version "1.7.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/svu/releases/download/v1.6.1/svu_1.6.1_darwin_amd64.tar.gz"
-      sha256 "626f7b2e13023e9b9135cf8ebd5699f9d29bd881e6bda7d9bd912ef373f6d06c"
+      url "https://github.com/caarlos0/svu/releases/download/v1.7.0/svu_1.7.0_darwin_amd64.tar.gz"
+      sha256 "7f9cb5d69c53eba815704bed87c91245988bee97299f9ee0fc9164527e3d8777"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/caarlos0/svu/releases/download/v1.6.1/svu_1.6.1_darwin_arm64.tar.gz"
-      sha256 "9a11f43965087118790c64075b769ecbcc6e5eca7d184ed3a85c4bf1b7baf4fb"
+      url "https://github.com/caarlos0/svu/releases/download/v1.7.0/svu_1.7.0_darwin_arm64.tar.gz"
+      sha256 "9777a2c8fbc34ba20458bb5f6ca9ea4c8658d21c8a24b9e385571d5a406fff4d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/svu/releases/download/v1.6.1/svu_1.6.1_linux_amd64.tar.gz"
-      sha256 "e6aca9765cfe42ec7e39bf2b1618f3bc29b3a84be4f9155f289c4604f8215cb2"
+      url "https://github.com/caarlos0/svu/releases/download/v1.7.0/svu_1.7.0_linux_amd64.tar.gz"
+      sha256 "e192c3d011f638e4a000ba5d3880d92e7b1164669d4eec8ce9e6dd84f44a3106"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/caarlos0/svu/releases/download/v1.6.1/svu_1.6.1_linux_arm64.tar.gz"
-      sha256 "e183a36d22a23db7957baa1f080121301e692277b3d988f963ed5cf05267d676"
+      url "https://github.com/caarlos0/svu/releases/download/v1.7.0/svu_1.7.0_linux_arm64.tar.gz"
+      sha256 "d96120800f0923cc50d3e51c274b691fcbc1d7f01b51636d2593c6fa896d581c"
     end
   end
 
