db4a56cdc892430182eadde3b991a08f95dd61a5
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-11 13:27:56 -0300

    Brew formula update for tt version v1.9.0

--- Formula/tt.rb
+++ Formula/tt.rb
@@ -5,28 +5,28 @@
 class Tt < Formula
   desc "Task Timer (tt) is a dead simple TUI task timer"
   homepage "https://github.com/caarlos0/tasktimer"
-  version "1.8.0"
+  version "1.9.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/tasktimer/releases/download/v1.8.0/tt_darwin_amd64.tar.gz"
-      sha256 "959b7ddcbccbdaf7c270fb5935da235faa15e9328283ffefe5c7910c458568d1"
+      url "https://github.com/caarlos0/tasktimer/releases/download/v1.9.0/tt_darwin_amd64.tar.gz"
+      sha256 "758aad0b474e75aa24ebf0d958ea73f30b261bfdf243056276f764bb6137f4e5"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/caarlos0/tasktimer/releases/download/v1.8.0/tt_darwin_arm64.tar.gz"
-      sha256 "7452cbbd66e4eafde8d8a93b108e99287401c062f0a03d6ccc76a33411cdc0d1"
+      url "https://github.com/caarlos0/tasktimer/releases/download/v1.9.0/tt_darwin_arm64.tar.gz"
+      sha256 "2290587ac992d44e9490d4c3eada1a36a31d8a040eeb182f22e25a0328f8665a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/tasktimer/releases/download/v1.8.0/tt_linux_amd64.tar.gz"
-      sha256 "4d90c92918922bc1203b5d49041b54cff9cac5e96928d2ddd9f8fa22ab89a9c5"
+      url "https://github.com/caarlos0/tasktimer/releases/download/v1.9.0/tt_linux_amd64.tar.gz"
+      sha256 "66e7d91d9a6bb3377f4cf11b751fb384484906132b31a1f0088373b2b1c29f96"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/caarlos0/tasktimer/releases/download/v1.8.0/tt_linux_arm64.tar.gz"
-      sha256 "d9248b873a268c34d97918cdc09b6365ec58c0e6f4f2d50b3afb5a9cd97f94a2"
+      url "https://github.com/caarlos0/tasktimer/releases/download/v1.9.0/tt_linux_arm64.tar.gz"
+      sha256 "ab2d652a64c161f2a90bdb7e3682b7cf586f37866fef1ef920db61ee6894236c"
     end
   end
 
