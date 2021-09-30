f04f968ab8983302447da54e91528dc7744537bc
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-29 22:49:16 -0300

    Brew formula update for kubectx version v0.10.1

--- Formula/kubectx.rb
+++ Formula/kubectx.rb
@@ -5,28 +5,28 @@
 class Kubectx < Formula
   desc "kubectx and kubens are power tools for kubectl"
   homepage "https://kubectx.dev"
-  version "0.10.0"
+  version "0.10.1"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.0/kubectx_v0.10.0_darwin_x86_64.tar.gz"
-      sha256 "5ef2a73ccd5c1ea2beac50c4c1789cb97f49c8b57a8c265137be2397a134ce14"
+      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.1/kubectx_v0.10.1_darwin_amd64.tar.gz"
+      sha256 "566262c5009bd305fcf852d475964a8c56ad72cc065b5c6e2e7928dfef2b02e0"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.0/kubectx_v0.10.0_darwin_arm64.tar.gz"
-      sha256 "980730fe2222d8a13f4562d2eefed1713009d9158d277df85019171b1cd21f67"
+      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.1/kubectx_v0.10.1_darwin_arm64.tar.gz"
+      sha256 "b0196851c196533d4235fcac246f98e60eb2369fcf876d06da9a967af245ab6e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.0/kubectx_v0.10.0_linux_x86_64.tar.gz"
-      sha256 "920e7a9e321f7430e1b53a0fd0fad91f5168dd6cdefc89ed22712b90b951412a"
+      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.1/kubectx_v0.10.1_linux_amd64.tar.gz"
+      sha256 "8eba3a77a958fbb74c1eae136ccd64b91bb2870e957a33d4c611d9e730f0689c"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.0/kubectx_v0.10.0_linux_arm64.tar.gz"
-      sha256 "b4593a54478ee8bc9723e0308c7f6646793233e98f743b1d3f8396b13475b44e"
+      url "https://github.com/caarlos0/kubectx/releases/download/v0.10.1/kubectx_v0.10.1_linux_arm64.tar.gz"
+      sha256 "4f0d3d7ac3a240354c43080b5f5c1d695d7e2e96f72fcb2afc88c4b6e1181d0a"
     end
   end
 
