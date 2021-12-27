2e62a31778c86318443624438a06a01d25abeb43
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-27 15:45:27 -0300

    Brew formula update for svu version v1.9.0

--- Formula/svu.rb
+++ Formula/svu.rb
@@ -5,31 +5,33 @@
 class Svu < Formula
   desc "Semantic Version Utility"
   homepage "https://github.com/caarlos0/svu"
-  version "1.8.0"
+  version "1.9.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/caarlos0/svu/releases/download/v1.8.0/svu_1.8.0_darwin_arm64.tar.gz"
-      sha256 "9aa510f6e630aa2f33467e234f064947148c8a7f5a47d18f14f680b3acfccdd9"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/svu/releases/download/v1.8.0/svu_1.8.0_darwin_amd64.tar.gz"
-      sha256 "64aaf972522846dfde706204aadfd9f3195ee6c6e2e62aec294b95ac49902c0d"
+    url "https://github.com/caarlos0/svu/releases/download/v1.9.0/svu_1.9.0_darwin_all.tar.gz"
+    sha256 "e2794744a5f0d3333024964f886c1ea32dd0cea478ab6467c9a50427f32a9bcc"
+
+    def install
+      bin.install "svu"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/caarlos0/svu/releases/download/v1.8.0/svu_1.8.0_linux_arm64.tar.gz"
-      sha256 "048be8ca7c0edc8ad9b666c849323a3b6d821a3ab5174aad381f77db4cde061d"
+      url "https://github.com/caarlos0/svu/releases/download/v1.9.0/svu_1.9.0_linux_arm64.tar.gz"
+      sha256 "10e71d5fb4864babf76d5decbab3e42c2656cbc0dd029763d9684acb7e065409"
+
+      def install
+        bin.install "svu"
+      end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/caarlos0/svu/releases/download/v1.8.0/svu_1.8.0_linux_amd64.tar.gz"
-      sha256 "5d1d3544ec6042c29cb9623247a1284194cabd7c1445d1bc144046d3c1cf6572"
-    end
-  end
+      url "https://github.com/caarlos0/svu/releases/download/v1.9.0/svu_1.9.0_linux_amd64.tar.gz"
+      sha256 "f9f37c9295bb2441c52f77bd79297ddd8029be7c65206b89c64e0778a1f0a195"
 
-  def install
-    bin.install "svu"
+      def install
+        bin.install "svu"
+      end
+    end
   end
 end
